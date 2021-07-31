e5cec5151d38d0bb832ca606019a990dd32aec66
MOHAMMAD <m.ayazadeh@gmail.com>
2021-07-31 20:33:49 +0430

    add models of product app to admin file

--- product/admin.py
+++ product/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from product.models import *
 
 admin.site.register(Product)
+admin.site.register(Price)
+admin.site.register(Discount)
+admin.site.register(Category)
