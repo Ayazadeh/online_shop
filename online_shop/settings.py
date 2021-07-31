3281a8109379f4cf7b41eea64739d707980dce2d
MOHAMMAD <m.ayazadeh@gmail.com>
2021-07-31 13:14:39 +0430

    configuration postgresql

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-eg&ui^vs@*_3z1%91n4o9h$t*1gqgt15aw!3kv6c$%)+3pe_!8
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,21 +69,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'online_shop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'vw2iulrc',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db.online_shop',
+        'USER': 'postgres',
+        'PASSWORD': 'vw2iulrc',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -118,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
