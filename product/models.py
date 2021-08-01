9ff665fa75d4fd4be874bb776682bc212436b913
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-01 23:37:24 +0430

    modified

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from core.models import *
 from django.utils.translation import gettext_lazy as _
 from product.validators import *
+from django.core.exceptions import ValidationError
 
 
 class Price(TimestampMixin):
