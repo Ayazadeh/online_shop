3eb13a363cffaf18ebc4f03ce6055a7f2680297c
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-01 21:17:36 +0430

    customization

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from product.models import *
 
-admin.site.register(Product)
-admin.site.register(Price)
-admin.site.register(Discount)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product_name', 'price', 'inventory', 'discount', 'brand', 'category']
+
+
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'price']
+
+
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ['id', 'discount_name', 'amount', 'unit', 'description']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category_name', 'parent']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Price, PriceAdmin)
+admin.site.register(Discount, DiscountAdmin)
+admin.site.register(Category, CategoryAdmin)
