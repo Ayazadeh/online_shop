a9315c55e1143423f4a0210e04a07d0c1f985e74
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-02 22:45:13 +0430

    customizing admin panel

--- product/admin.py
+++ product/admin.py
@@ -4,6 +4,7 @@ from product.models import *
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'product_name', 'price', 'inventory', 'discount', 'brand', 'category']
+    search_fields = ['product_name', 'brand']
 
 
 class PriceAdmin(admin.ModelAdmin):
@@ -12,13 +13,21 @@ class PriceAdmin(admin.ModelAdmin):
 
 class DiscountAdmin(admin.ModelAdmin):
     list_display = ['id', 'discount_name', 'amount', 'unit', 'description']
+    search_fields = ['discount_name', 'unit']
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'category_name', 'parent']
+    search_fields = ['category_name']
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['id', 'brand_name']
+    search_fields = ['brand_name']
 
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Price, PriceAdmin)
 admin.site.register(Discount, DiscountAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
