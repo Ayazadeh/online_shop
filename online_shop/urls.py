90724f1a29399900e58a5488dda4397bfb0a3660
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-02 22:44:31 +0430

    add urls of home app

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('customer.urls')),
+    path('', include('home.urls'))
 ]
 
 
