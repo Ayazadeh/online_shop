441f44109030bf8c1126ba0e96e7e0dad49ac143
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-03 19:45:43 +0430

    add Address model

--- customer/models.py
+++ customer/models.py
@@ -4,3 +4,12 @@ from core.models import *
 class Customer(User, TimestampMixin):
     pass
 
+
+class Address(TimestampMixin):
+    state = models.CharField(max_length=30,
+                             null=False,
+                             blank=False)
+    city = models.CharField(max_length=30,
+                            null=False,
+                            blank=False)
+
