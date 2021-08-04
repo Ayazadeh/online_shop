fe3620d6a94acb462f3fad283b54668da914868e
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-04 17:23:59 +0430

    add some test for deleted

--- core/tests.py
+++ core/tests.py
@@ -7,7 +7,18 @@ class BaseModelTest(TestCase):
     def setUp(self) -> None:
         self.m1 = TestModel.objects.create()
 
-    def test1(self):
+    def test1_get_deleted(self):
         self.m1.deleted = True
         self.m1.save()
-        self.assertEqual(self.m1.deleted, True)
+        self.assertNotIn(self.m1, TestModel.objects.all())
+
+    def test2_filter_deleted(self):
+        self.m1.deleted = True
+        self.m1.save()
+        self.assertNotIn(self.m1, TestModel.objects.filter())
+
+    def test4_archive(self):
+        self.m1.deleted = True
+        self.m1.save()  # Deleted !!!
+        self.assertIn(self.m1, TestModel.objects.archive())
+        self.assertNotIn(self.m1, TestModel.objects.all())
