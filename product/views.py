cb0b1d9aca1610a40f9ff5d6a6814ca3093793ed
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-04 14:32:42 +0430

    modified view for product

--- product/views.py
+++ product/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from product.models import Product
 
 
-class ProductView(DetailView):
+class ProductView(ListView):
     model = Product
     template_name = "product.html"
