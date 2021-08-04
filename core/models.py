26a0f05906e75f0b33d8fd19c22205c4ddbf9fa3
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-04 19:17:06 +0430

    modified

--- core/models.py
+++ core/models.py
@@ -24,14 +24,14 @@ class TimestampMixin(BaseModel):
     class Meta:
         abstract = True
 
-    Create_timestamp = models.DateTimeField(auto_now_add=True)
-    Modify_timestamp = models.DateTimeField(auto_now=True)
-    Delete_timestamp = models.DateTimeField(default=False,
+    create_timestamp = models.DateTimeField(auto_now_add=True)
+    modify_timestamp = models.DateTimeField(auto_now=True)
+    delete_timestamp = models.DateTimeField(default=False,
                                             null=True,
                                             blank=True, )
 
     def logical_delete(self):
-        self.Delete_timestamp = timezone.now()
+        self.delete_timestamp = timezone.now()
         self.save()
 
 
