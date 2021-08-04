79253d1dd938201d21dadf1ba917924eb6cf6230
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-04 14:35:49 +0430

    add static for image's in media directory

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -1,10 +1,13 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from online_shop import settings
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('customer.urls')),
-    path('', include('home.urls'))
-]
-
-
+                  path('admin/', admin.site.urls),
+                  path('accounts/', include('customer.urls')),
+                  path('', include('home.urls')),
+                  path('order/', include('order.urls')),
+                  path('product/', include('product.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
