45b447c517f5714e016a44da191b600eb467e8b1
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-05 20:25:15 +0430

    add rest_framework to INSTALLED_APPS

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
     'core',
     'customer',
     'home',
