bb9c4e6c68093fef92abdb6aa8a2bbb9a09ea305
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-05 23:14:38 +0430

    add product model to home view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from product.models import Product
 
 
-class LandingPage(TemplateView):
+class LandingPage(ListView):
+    model = Product
     template_name = 'index.html'
