60fa6975c508aaa1fba41db1b37561e951933ffd
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-05 23:11:57 +0430

    change default of delete timestamp to None

--- core/models.py
+++ core/models.py
@@ -26,9 +26,9 @@ class TimestampMixin(BaseModel):
 
     create_timestamp = models.DateTimeField(auto_now_add=True)
     modify_timestamp = models.DateTimeField(auto_now=True)
-    delete_timestamp = models.DateTimeField(default=False,
+    delete_timestamp = models.DateTimeField(default=None,
                                             null=True,
-                                            blank=True, )
+                                            blank=True,)
 
     def logical_delete(self):
         self.delete_timestamp = timezone.now()
