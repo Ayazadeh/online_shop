68773af8b301c8c381fbd88572ad62be8ae77906
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-05 19:26:36 +0430

    add test for Discount model

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from product.models import *
 
-# Create your tests here.
+
+class DiscountTest(TestCase):
+
+    def test1_unit_percent(self):
+        self.dis1 = Discount.objects.create(amount=0, unit='percent')
+        self.dis2 = Discount.objects.create(amount=20, unit='percent')
+
+    def test2_unit_rial(self):
+        self.dis1 = Discount.objects.create(amount=0, unit='rial')
+        self.dis2 = Discount.objects.create(amount=20, unit='rial')
+
+    def test3_amount_negative(self):
+        # This code should give an error
+        self.dis1 = Discount.objects.create(amount=-20, unit='rial')
+        self.dis1 = Discount.objects.create(amount=-20, unit='percent')
+
+
+# class ProductTest(TestCase):
+#
+#     def setUp(self) -> None:
+#         self.discount_1 = Discount.objects.create(
+#             amount=20,
+#             unit='percent'
+#         )
+#         self.category_1 = Category.objects.create(
+#             category_name='phone'
+#         )
+#         self.brand_1 = Brand.objects.create(
+#             brand_name='LG'
+#         )
+#         self.product_1 = Product.objects.create(
+#             product_name='k4',
+#             price=1000000,
+#             brand=self.brand_1,
+#             discount=self.discount_1,
+#             category=self.category_1,
+#             product_image=None,
+#             inventory=2
+#         )
+#
+#     def test1_final_price(self):
+#         self.assertEqual(self.product_1.final_price(), 7000000)
