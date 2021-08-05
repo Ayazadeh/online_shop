7dd8702d44a35154bfcca0665030886133e025ba
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-05 23:12:42 +0430

    add app name to home urls

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from home.views import *
 
+app_name = 'home'
 urlpatterns = [
-                  path('', LandingPage.as_view())
-              ]
-
+    path('', LandingPage.as_view(), name='landing_page')
+]
