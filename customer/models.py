1b5288ef25353da8e731139b2177327b927e3b63
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-06 17:24:28 +0430

    add customer and address models

--- customer/models.py
+++ customer/models.py
@@ -1,15 +1,25 @@
 from core.models import *
 
 
-class Customer(User, TimestampMixin):
-    pass
+class Customer(TimestampMixin):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Address(TimestampMixin):
+    owner = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
     state = models.CharField(max_length=30,
                              null=False,
                              blank=False)
+
     city = models.CharField(max_length=30,
                             null=False,
                             blank=False)
 
+    lat = models.FloatField()
+    lng = models.FloatField()
+    detail = models.CharField(max_length=100,
+                              null=True,
+                              blank=True)
+    zip_code = models.PositiveIntegerField()
+    plaque = models.PositiveIntegerField()
