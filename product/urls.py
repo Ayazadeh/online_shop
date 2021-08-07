36d7f92f0fa6cef2d225aa20664a2c531de4f9a1
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-07 20:18:48 +0430

    add url for category and category detail

--- product/urls.py
+++ product/urls.py
@@ -4,6 +4,9 @@ from product.views import *
 app_name = 'product'
 urlpatterns = [
     path('', ProductView.as_view(), name='product'),
+    path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
     path('api/', ProductListApiView.as_view(), name='api'),
     path('api/<str:pk>', ProductDetailApi.as_view(), name='api_detail'),
+    path('category/', CategoryListView.as_view(), name='category'),
+    path('category/<int:pk>', CategoryDetailView.as_view(), name='category_detail')
 ]
