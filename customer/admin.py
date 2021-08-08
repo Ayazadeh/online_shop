2626293d471595fc5793760d300f06e47aa6f13f
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-08 20:30:14 +0430

    customize admin customer

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from customer.models import *
+
 
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user']
+    search_fields = ['user__username']
+
+
+admin.site.register(Customer, CustomerAdmin)
