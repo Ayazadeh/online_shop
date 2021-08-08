fb691de1f946864063bb93588b74465e11985220
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-08 20:39:46 +0430

    add method for list display

--- customer/admin.py
+++ customer/admin.py
@@ -5,8 +5,14 @@ from customer.models import *
 # Register your models here.
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user']
+    list_display = ['id', 'user', 'email', 'super_user']
     search_fields = ['user__username']
+
+    def email(self, obj):
+        return obj.user.email
+
+    def super_user(self, obj):
+        return obj.user.is_superuser
 
 
 admin.site.register(Customer, CustomerAdmin)
