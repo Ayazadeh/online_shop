ab4b98fcd1ed87a80aaa1dffa418e738b7db9bf9
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-09 00:00:29 +0430

    add image format

--- customer/validators.py
+++ customer/validators.py
@@ -4,6 +4,6 @@ from django.core.exceptions import ValidationError
 
 def validate_file_extension(value):
     ext = os.path.splitext(value.name)[1]
-    valid_extensions = ['.jpg', '.png']
+    valid_extensions = ['.jpg', '.png', '.jpeg']
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
