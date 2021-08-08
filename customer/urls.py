28e8572e9122ce7dba7b76613762665b74fdaad2
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-08 20:06:12 +0430

    add api url's

--- customer/urls.py
+++ customer/urls.py
@@ -4,5 +4,9 @@ from customer.views import *
 urlpatterns = [
     path('login/', Login.as_view(), name='my_login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('logout/', Logout.as_view(), name='logout')
+    path('logout/', Logout.as_view(), name='logout'),
+    path('user_api/', UserListApi.as_view(), name='users'),
+    path('user_api/<int:pk>', UserDetailApi.as_view(), name='user_detail'),
+    path('address_api/', AddressListApi.as_view(), name='address'),
+    path('address_api/<int:pk>', AddressDetailApi.as_view(), name='address_detail')
 ]
