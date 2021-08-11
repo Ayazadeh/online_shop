566fba4ca912e4a595ab38dff69602a7a245c536
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-12 00:12:42 +0430

    add order item for admin

--- order/admin.py
+++ order/admin.py
@@ -7,8 +7,13 @@ class OrderStatusAdmin(admin.ModelAdmin):
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'number']
+    list_display = ['id', 'customer']
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'ordered', 'product', 'quantity']
 
 
 admin.site.register(OrderStatus, OrderStatusAdmin)
 admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
