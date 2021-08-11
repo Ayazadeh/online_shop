817a59a8d439c31ab004e81909638c8a412f007e
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-12 00:07:58 +0430

    add some test for customer and address

--- customer/tests.py
+++ customer/tests.py
@@ -4,8 +4,38 @@ from customer.models import *
 
 class CustomerTest(TestCase):
 
-    def setUp(self):
-        user = User.objects.create(password='1234', )
-
     def test1_customer_obj(self):
-        Customer.objects.create()
+        self.test = Customer.objects.create(
+            username='mohammad',
+            email='m.ayazadeh@gmail.com',
+            password='123456789',
+            phone='09038586843'
+        )
+
+    def test2_customer_obj(self):
+        self.test = Customer.objects.create(
+            username='mohammad',
+            email='m.ayazadeh@gmail.com',
+            password='123456789',
+            phone='038586843'
+        )
+
+
+class AddressTest(TestCase):
+
+    def setUp(self):
+        self.test = Customer.objects.create(
+            username='mohammad',
+            email='m.ayazadeh@gmail.com',
+            password='123456789',
+            phone='09038586843'
+        )
+
+    def test1_address_obj(self):
+        self.address_test = Address.objects.create(
+            owner=self.test,
+            state='Tehran',
+            city='tehran',
+            zip_code=123456,
+            plaque=10
+        )
