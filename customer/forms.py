40155ad2bfac360e4dc6fdc9d890e0ad15717a1d
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-11 14:33:34 +0430

    add RegisterForm for customer

--- customer/forms.py
+++ customer/forms.py
@@ -1,4 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from customer.models import *
 
 
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50,
+                                 required=True)
 
+    last_name = forms.CharField(max_length=50,
+                                required=True)
+
+    class Meta:
+        model = Customer
+        fields = ['username',
+                  'first_name',
+                  'last_name',
+                  'email',
+                  'password1',
+                  'password2',
+                  'customer_image']
