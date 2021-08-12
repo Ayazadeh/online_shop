346870ff41a92ee3e1bc5adc5741cab63c0c7607
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-12 20:13:36 +0430

    add middleware file to setting

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'customer.middleware.CustomClassMiddleware',
 ]
 
 ROOT_URLCONF = 'online_shop.urls'
