7ec7968166090abc0f737012ce91bec31326f0b3
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-13 21:33:29 +0430

    add validator for zip code

--- customer/validators.py
+++ customer/validators.py
@@ -15,3 +15,9 @@ def phone_validation(phone):
     pattern = r'(^09\d{9}$)|(^\+989\d{9}$)'
     if not bool(re.match(pattern, phone)):
         raise ValidationError('Phone number is wrong!!!')
+
+
+def zip_code_validation(zip_code):
+    pattern = r'\d{10}'
+    if not bool(re.match(pattern, zip_code)):
+        raise ValidationError('zip_code most be 10 character and just number!!!')
