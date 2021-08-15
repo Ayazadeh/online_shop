b5568d5416259248de530225532302a553e840ee
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-15 23:18:41 +0430

    add url for cart

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from order.views import add_to_cart, remove_from_cart
+from order.views import *
 
 app_name = 'order'
 
 urlpatterns = [
     path('add-to-cart/<int:pk>', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<int:pk>', remove_from_cart, name='remove-from-cart'),
+    path('cart/', Cart.as_view(), name='cart')
 ]
