2a4ec6950859fcb2140269bd8e70689c4bc0c120
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-15 23:17:56 +0430

    add some test for customer and address

--- customer/tests.py
+++ customer/tests.py
@@ -4,38 +4,67 @@ from customer.models import *
 
 class CustomerTest(TestCase):
 
-    def test1_customer_obj(self):
-        self.test = Customer.objects.create(
+    def setUp(self):
+        self.customer = Customer.objects.create(
             username='mohammad',
             email='m.ayazadeh@gmail.com',
             password='123456789',
             phone='09038586843'
         )
+        self.customer.full_clean()
 
-    def test2_customer_obj(self):
-        self.assertRaises(Exception, Customer.objects.create(
-            username='mohammad',
-            email='m.ayazadeh@gmail.com',
-            password='123456789',
-            phone='123'
-        ))
+    def test1_customer_email(self):
+        self.customer.email = 'm.gmail.com'
+        self.assertRaises(ValidationError, self.customer.full_clean)
+
+    def test2_customer_email(self):
+        self.customer.email = 'm.@gmail'
+        self.assertRaises(ValidationError, self.customer.full_clean)
+
+    def test3_customer_email(self):
+        self.customer.email = '@gmail.com'
+        self.assertRaises(ValidationError, self.customer.full_clean)
+
+    def test4_customer_obj(self):
+        self.customer.phone = '123'
+        self.assertRaises(ValidationError, self.customer.full_clean)
+
+    def test5_customer_obj(self):
+        self.customer.phone = '12345678901'
+        self.assertRaises(ValidationError, self.customer.full_clean)
+
+    def test6_customer_obj(self):
+        self.customer.phone = '91234567890'
+        self.assertRaises(ValidationError, self.customer.full_clean)
 
 
 class AddressTest(TestCase):
 
     def setUp(self):
-        self.test = Customer.objects.create(
+        self.customer = Customer.objects.create(
             username='mohammad',
             email='m.ayazadeh@gmail.com',
             password='123456789',
             phone='09038586843'
         )
-
-    def test1_address_obj(self):
         self.address_test = Address.objects.create(
-            owner=self.test,
+            owner=self.customer,
             state='Tehran',
             city='tehran',
-            zip_code=123456,
+            zip_code=1234567890,
             plaque=10
         )
+        self.customer.full_clean()
+        self.address_test.full_clean()
+
+    def test1_address_zip_code(self):
+        self.address_test.zip_code = '123'
+        self.assertRaises(ValidationError, self.address_test.full_clean)
+
+    def test2_address_zip_code(self):
+        self.address_test.zip_code = 'zip code'
+        self.assertRaises(ValidationError, self.address_test.full_clean)
+
+    def test3_address_zip_code(self):
+        self.address_test.zip_code = '12zipcode.'
+        self.assertRaises(ValidationError, self.address_test.full_clean)
