f3b0ddf018e29d69add8154237b8dbac034bce42
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-16 14:50:39 +0430

    modified UserAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from core.models import *
-
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'username', 'email', 'is_superuser', 'is_staff']
-    search_fields = ['id', 'username']
-
 
 admin.site.register(User, UserAdmin)
