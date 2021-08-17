7066016e02b63055c83a0eeab49caa7badc3d8c3
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-17 12:38:15 +0430

    add fields to admin

--- product/admin.py
+++ product/admin.py
@@ -3,23 +3,63 @@ from product.models import *
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'product_name', 'price', 'inventory', 'discount', 'brand', 'category']
-    search_fields = ['product_name', 'brand']
+    list_display = [
+        'id',
+        'product_name',
+        'price',
+        'inventory',
+        'discount',
+        'brand',
+        'category'
+    ]
+
+    search_fields = [
+        'product_name',
+        'price',
+        'brand',
+        'inventory'
+    ]
+
+    fields = [
+        'product_name',
+        'price',
+        'discount',
+        'category',
+        'inventory',
+        'brand',
+        'product_image'
+    ]
 
 
 class DiscountAdmin(admin.ModelAdmin):
-    list_display = ['id', 'discount_name', 'amount', 'unit', 'description']
+    list_display = [
+        'id',
+        'discount_name',
+        'amount',
+        'unit',
+        'description'
+    ]
+
     search_fields = ['discount_name', 'unit']
+
+    fields = [
+        'discount_name',
+        'amount',
+        'unit',
+        'description',
+    ]
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'category_name', 'parent']
     search_fields = ['category_name']
+    fields = ['category_name', 'parent']
 
 
 class BrandAdmin(admin.ModelAdmin):
     list_display = ['id', 'brand_name']
     search_fields = ['brand_name']
+    fields = ['brand_name']
 
 
 admin.site.register(Product, ProductAdmin)
