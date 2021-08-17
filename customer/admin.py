a615b44258aac83c2fdf60d66b33e844910c63ee
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-17 12:14:37 +0430

    add fieldsets to customer and address

--- customer/admin.py
+++ customer/admin.py
@@ -3,16 +3,41 @@ from customer.models import *
 
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['user_ptr_id', 'username', 'email', 'is_superuser', 'phone', 'is_active']
-    search_fields = ['username', 'phone', 'first_name', 'last_name']
-    exclude = ['last_login',
-               'is_superuser',
-               'groups',
-               'user_permissions',
-               'is_staff',
-               'is_active',
-               'date_joined',
-               '']
+    list_display = [
+        'user_ptr_id',
+        'username',
+        'email',
+        'is_superuser',
+        'phone',
+        'is_active'
+    ]
+    search_fields = [
+        'username',
+        'phone',
+        'first_name',
+        'last_name'
+    ]
+    exclude = [
+        'last_login',
+        'is_superuser',
+        'groups',
+        'user_permissions',
+        'is_staff',
+        'is_active',
+        'date_joined',
+    ]
+    fieldsets = [
+        (None,
+         {'fields': [
+             'username',
+             'first_name',
+             'last_name',
+             'email',
+             'phone',
+             'password',
+             'customer_image'
+         ]})
+    ]
 
     # def save_model(self, request, obj, form, change):
     #     obj.set_password(obj.password)
@@ -20,12 +45,38 @@ class CustomerAdmin(admin.ModelAdmin):
 
 
 class AddressAdmin(admin.ModelAdmin):
-    list_display = ['id', 'owner_address', 'state', 'city', 'zip_code', 'plaque', 'lat', 'lng']
-    search_fields = ['owner_address', 'state', 'city', 'plaque']
+    list_display = [
+        'id',
+        'owner_address',
+        'state', 'city',
+        'zip_code',
+        'plaque',
+        'lat',
+        'lng'
+    ]
+    search_fields = [
+        'owner_address',
+        'state', 'city',
+        'plaque'
+    ]
 
     @staticmethod
     def owner_address(obj):
         return obj.owner.username
+
+    fieldsets = [
+        (None,
+         {'fields': [
+             'owner',
+             'state',
+             'city',
+             'detail',
+             'zip_code',
+             'plaque',
+             'lat',
+             'lng'
+         ]})
+    ]
 
 
 admin.site.register(Customer, CustomerAdmin)
