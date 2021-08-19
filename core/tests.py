c44354761d1f82ee6502960b4eaaa6dc4d8cd1cf
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-19 13:53:36 +0430

    change deleted to is_deleted

--- core/tests.py
+++ core/tests.py
@@ -8,17 +8,17 @@ class BaseModelTest(TestCase):
         self.m1 = TestModel.objects.create()
 
     def test1_get_deleted(self):
-        self.m1.deleted = True
+        self.m1.is_deleted = True
         self.m1.save()
         self.assertNotIn(self.m1, TestModel.objects.all())
 
     def test2_filter_deleted(self):
-        self.m1.deleted = True
+        self.m1.is_deleted = True
         self.m1.save()
         self.assertNotIn(self.m1, TestModel.objects.filter())
 
     def test4_archive(self):
-        self.m1.deleted = True
+        self.m1.is_deleted = True
         self.m1.save()  # Deleted !!!
         self.assertIn(self.m1, TestModel.objects.archive())
         self.assertNotIn(self.m1, TestModel.objects.all())
