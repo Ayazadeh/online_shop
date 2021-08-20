ea814c8305ad36091da2e3005d0815f11a782731
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-20 22:58:51 +0430

    add serializers for order and order item

---
+++ order/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from order.models import *
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
