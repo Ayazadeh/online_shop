a7a3bbbf2b0f367c35be7a9c89fdda7673c209c4
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-21 17:44:28 +0430

    add add to cart to url's of order

--- order/urls.py
+++ order/urls.py
@@ -5,6 +5,6 @@ app_name = 'order'
 
 urlpatterns = [
     path('add-to-cart/<int:pk>', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<int:pk>', remove_from_cart, name='remove-from-cart'),
+    # path('remove-from-cart/<int:pk>', remove_from_cart, name='remove-from-cart'),
     path('cart/', Cart.as_view(), name='cart')
 ]
