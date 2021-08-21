a0f14ba364a955d66fa8f50c869141b097355514
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-22 04:02:53 +0430

    will be change

--- order/views.py
+++ order/views.py
@@ -17,18 +17,19 @@ class Cart(LoginRequiredMixin, views.View):
         return render(request, 'order/cart.html')
 
 
-# def add_to_cart(request, **kwargs):
-#     customer = get_object_or_404(Customer, user_ptr_id=request.user.id)
-#     product = Product.objects.filter(id=kwargs.get('item_id', "")).first()
-#     order_item, status = OrderItem.objects.get_or_create(product=product)
-#     customer_order, status = Order.objects.get_or_create(owner=customer, is_ordered=True)
-#     customer_order.items.add(order_item)
-#     if status:
-#         customer_order.ref_code = generate_order_id()
-#         customer_order.save()
-#
-#     messages.info(request, "item added to cart")
-#     return redirect(reverse('product:product'))
+def add_to_cart(request, **kwargs):
+    customer = get_object_or_404(Customer, user_ptr_id=request.user.id)
+    product = Product.objects.filter(id=kwargs.get('pk', "")).first()
+    print(product)
+    print(kwargs.get('pk'))
+    order_item, status = OrderItem.objects.get_or_create(product=product)
+    customer_order, status = Order.objects.get_or_create(owner=customer, is_ordered=True)
+    customer_order.items.add(order_item)
+    if status:
+        customer_order.save()
+
+    messages.info(request, "item added to cart")
+    return redirect(reverse('product:product'))
 #
 #
 # def delete_from_cart(request, item_id):
@@ -45,35 +46,35 @@ class Cart(LoginRequiredMixin, views.View):
 #         'order': existing_order
 #     }
 #     return render(request, 'order/cart.html', context)
-def add_to_cart(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    order_item, created = OrderItem.objects.get_or_create(
-        product=product,
-        is_ordered=False
-    )
-
-    order_query_set = Order.objects.filter(owner=request.user, is_ordered=False)
-
-    if order_query_set.exists():
-        order = order_query_set[0]
-
-        if Order.items.filter(order__items__product_id=product.id).exists():
-            order_item.quantity += 1
-            order_item.save()
-            messages.info(request, "Added quantity Item")
-            return redirect("product:product_detail", pk=pk)
-        else:
-            Order.items.add(order_item)
-            messages.info(request, "Item added to your cart")
-            return redirect("product:product_detail", pk=pk)
-    else:
-        ordered_date = timezone.now()
-        order = Order.objects.create(owner_id=request.user.id,
-                                     ordered_date=ordered_date,
-                                     )
-        order.items.add(order_item)
-        messages.info(request, "Item added to your cart")
-        return redirect('product:product_detail', pk=pk)
+# def add_to_cart(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     order_item, created = OrderItem.objects.get_or_create(
+#         product=product,
+#         is_ordered=False
+#     )
+#
+#     order_query_set = Order.objects.filter(owner=request.user, is_ordered=False)
+#
+#     if order_query_set.exists():
+#         order = order_query_set[0]
+#
+#         if Order.items.filter(order__items__product_id=product.id).exists():
+#             order_item.quantity += 1
+#             order_item.save()
+#             messages.info(request, "Added quantity Item")
+#             return redirect("product:product_detail", pk=pk)
+#         else:
+#             Order.items.add(order_item)
+#             messages.info(request, "Item added to your cart")
+#             return redirect("product:product_detail", pk=pk)
+#     else:
+#         ordered_date = timezone.now()
+#         order = Order.objects.create(owner_id=request.user.id,
+#                                      ordered_date=ordered_date,
+#                                      )
+#         order.items.add(order_item)
+#         messages.info(request, "Item added to your cart")
+#         return redirect('product:product_detail', pk=pk)
 #
 #
 # def remove_from_cart(request, pk):
