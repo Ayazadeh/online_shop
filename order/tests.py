102387db4782fbdbb7d852c7719a463c968a2414
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-22 19:25:11 +0430

    add some test for order and order item

--- order/tests.py
+++ order/tests.py
@@ -1,9 +1,61 @@
 from django.test import TestCase
-from order.models import *
-from customer.models import *
+from customer.models import Customer
+from order.models import Order, OrderItem
+from product.models import *
 
 
-class OrderTest(TestCase):
+class Subtotal(TestCase):
+    def setUp(self) -> None:
+        self.customer = Customer.objects.create(
+            username='mohammad',
+            password='12345678',
+            email='m@gmail.com',
+            phone='09123456789'
+        )
+        print(self.customer)
+        self.discount_1 = Discount.objects.create(
+            amount=20,
+            unit='percent'
+        )
 
-    def setUp(self):
-        self.customer = Customer.objects.create()
+        self.category_1 = Category.objects.create(category_name='phone')
+
+        self.brand_1 = Brand.objects.create(brand_name='LG')
+
+        self.product_1 = Product.objects.create(
+            product_name='laptop',
+            price=30000000,
+            brand=self.brand_1,
+            discount=self.discount_1,
+            category=self.category_1,
+            product_image=None,
+            inventory=1
+        )
+        self.product_2 = Product.objects.create(
+            product_name='mobile',
+            price=5000000,
+            brand=self.brand_1,
+            discount=self.discount_1,
+            category=self.category_1,
+            product_image=None,
+            inventory=1
+        )
+        self.order_item_1 = OrderItem.objects.create(
+            product=self.product_1,
+            quantity=1
+        )
+        self.order_item_2 = OrderItem.objects.create(
+            product=self.product_2,
+            quantity=1
+        )
+        self.order = Order.objects.create(
+            owner=self.customer,
+            status='pay',
+        )
+        self.order.items.add(self.order_item_1, self.order_item_2)
+
+    def test1_total_price(self):
+        self.assertEqual(self.order.total_price(self.order_item_1.id, self.order_item_1.quantity), 24000000)
+
+    def test2_get_cart_total(self):
+        self.assertEqual(self.order.get_cart_total(), 28000000)
