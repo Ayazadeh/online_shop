efd34f3a560370f629a617d2a592c71e7d050df9
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-22 19:26:24 +0430

    modified url's of api in order app

--- order/urls.py
+++ order/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('add-to-cart/<int:pk>', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<int:pk>', remove_from_cart, name='remove-from-cart'),
     path('cart/', Cart.as_view(), name='cart'),
-    path('order/all/api', OrderListApi.as_view(), name='order_all_api'),
-    path('order/api/<int:pk>', OrderDetailApi.as_view(), name='order_detail_api'),
+    path('all/api', OrderListApi.as_view(), name='order_all_api'),
+    path('api/<int:pk>', OrderDetailApi.as_view(), name='order_detail_api'),
 ]
