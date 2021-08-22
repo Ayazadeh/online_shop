04e8fc10136fe6039483c7b601448cab6c9f7835
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-22 19:07:50 +0430

    delete ref_code from order and delete validator of quantity

--- order/models.py
+++ order/models.py
@@ -7,9 +7,7 @@ from product.models import Product
 
 
 class Order(TimestampMixin):
-    ref_code = models.CharField(max_length=15,
-                                verbose_name=_("reference code:"),
-                                help_text=_("add reference code for order"))
+
     owner = models.ForeignKey(Customer,
                               on_delete=models.SET_NULL,
                               null=True,
@@ -33,13 +31,14 @@ class Order(TimestampMixin):
     @staticmethod
     def total_price(product_id, count):
         price = Product.objects.get(id=product_id).final_price()
+        print('total price:', price * count)
         return price * count
 
     def get_cart_items(self):
         return self.items.all()
 
     def get_cart_total(self):
-        return sum([item.product.price for item in self.items.all()])
+        return sum([self.total_price(item.product.id, item.quantity) for item in self.items.all()])
 
     def __str__(self):
         return self.status
@@ -54,12 +53,7 @@ class OrderItem(models.Model):
     date_added = models.DateTimeField(auto_now=True)
     date_ordered = models.DateTimeField(null=True)
 
-    def number_product_exist(self):
-        if self.quantity > self.product.inventory:
-            raise ValidationError('inventory is not enough !!!')
-
-    quantity = models.PositiveIntegerField(default=1,
-                                           validators=[number_product_exist])
+    quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return f'{self.quantity} of {self.product.product_name}'
