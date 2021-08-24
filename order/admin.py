e6d5ab6fc557c3771424b05ca390f983f4a62112
MOHAMMAD <m.ayazadeh@gmail.com>
2021-08-24 15:20:49 +0430

    exclude some field in order admin

--- order/admin.py
+++ order/admin.py
@@ -4,10 +4,12 @@ from order.models import *
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'owner']
+    exclude = ['is_deleted', 'delete_timestamp']
 
 
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ['id', 'is_ordered', 'product', 'quantity']
+    exclude = ['is_deleted', 'delete_timestamp']
 
 
 admin.site.register(Order, OrderAdmin)
