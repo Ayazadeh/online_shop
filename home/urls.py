d9b6dd185010bf0cbf79d118bcf31fa9919e8c78
Ayazadeh <m.ayazadeh@gmail.com>
2022-01-07 09:03:32 -0800

    add about us page

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import ListView
-
+from . import views
 from product.models import Product
 
 app_name = 'home'
 urlpatterns = [
     path('', ListView.as_view(template_name='index.html', queryset=Product.objects.all()[:8]),
-         name='landing_page')
+         name='landing_page'),
+    path('about_us/', views.about_us, name='about_us'),
 ]
